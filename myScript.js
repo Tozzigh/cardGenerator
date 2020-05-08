8c3bdf1f90f95e7f0ab68024d06912445d347be0
Tozzigh <tozzigh@gmail.com>
2020-05-08 12:22:10 +0000

    Clean

--- myScript.js
+++ myScript.js
@@ -1,47 +1,20 @@
-var arrayColors = ["red", "black"];
 var arraySeeds = ["\u2660", "\u2663", "\u2665", "\u2666"];
 var arrayNumber =["1","2","3","4","5","6","7","8","9","10","J","Q","K"];
 
 window.onload = () =>{
+    createCard();
+}      
 
-        var randColor = random(arrayColors);
-        var randSeed = random(arraySeeds);
-        var randNumb = random(arrayNumber);
+function createCard(){
+    let number = random(arrayNumber);
+    let seed = random(arraySeeds);
+    let coloR = (seed == "\u2660") || (seed == "\u2663") ? "black" : "red";
 
-	    card = document.createElement("div");       
-        card.className = "cards";
-        card.style.height = "740px";
-        card.style.width = "550px";
-        card.style.borderRadius = "10%";
-        card.style.backgroundColor = "white";        
-
-        seed = document.createElement("div");   
-        seed.className = "seed";     
-        seed.style.fontSize = "200px";
-        colore = document.createTextNode(randSeed); 
-        seed.appendChild(colore);
-        seed.style.color = randColor;
-
-        seed2 = document.createElement("div");   
-        seed2.className = "seed2";
-        seed2.style.fontSize = "200px";
-        colore2 = document.createTextNode(randSeed);        
-        seed2.appendChild(colore2);
-        seed2.style.color = randColor;
-
-        numb = document.createElement("div");   
-        numb.className = "numb";
-        numb.style.fontSize = "300px";
-        numero = document.createTextNode(randNumb);        
-        numb.appendChild(numero);
-        numb.style.color = "black";
-
-        card.appendChild(numb);
-        card.appendChild(seed);
-        card.appendChild(seed2);
-        document.body.appendChild( card );
-    }   
-
+    let seedCard = document.querySelectorAll("span")[0]; seedCard.style.color = coloR; seedCard.innerHTML = seed;
+    let numberCard = document.querySelectorAll("span")[1].innerHTML = number; 
+    let seedCard1 = document.querySelectorAll("span")[2]; seedCard1.style.color = coloR; seedCard1.innerHTML = seed;
+    
+}
 function random(a){
     return a[Math.floor(Math.random() * a.length)]
 }
